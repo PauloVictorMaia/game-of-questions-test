cc2a2109b25997630e1c0b0f9326a65837bcce68
Paulo <pauloaspinfa@gmail.com>
2021-03-17 21:31:08 -0300

    rendered tests category list and button sin-in

--- src/pages/login/sin-in/index.js
+++ src/pages/login/sin-in/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Container, ActionButton, ActionText } from './styles';
-import SASIcon from '../../../assets/images/sas-image.svg';
+import SasIcon from '../../../assets/images/sas-image.svg';
 
 const SinIn = ({ navigation }) => {
   return (
     <Container>
-      <SASIcon />
-      <ActionButton onPress={() => navigation.navigate('ToPlay')}>
+      <SasIcon />
+      <ActionButton testID='button-sin-in' onPress={() => navigation.navigate('ToPlay')}>
         <ActionText>
           Sin In
         </ActionText>
